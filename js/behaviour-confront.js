87f0747005ff032e949b7555c775c32257a46af7
Mauricio Rivero <rivero.mauricio@gmail.com>
2023-02-13 15:04:08 -0500

    finished the behavior in the JS file

--- js/behaviour-confront.js
+++ js/behaviour-confront.js
@@ -10,20 +10,38 @@ document.getElementById("question_button").onclick = function(){
 
 function confrontarObjetoAdoracion(objeto){
     if(objeto===""){
-        alert("No me has dicho que amas");
+        document.getElementById("answer_idol").style.display = "flex";
+        document.getElementById("answeridol_output1").innerHTML="No has definido que es lo que mas amas";
     }else if(objeto==="Dios"){
         adorarDios();
     }else{
-        adorarIdolo();
+        adorarIdolo(objeto);
     }
 }
 
-function adorarIdolo(){
-    alert("Adoras a un idolo")
+function adorarIdolo(objeto){
+    document.getElementById("answer_idol").style.display = "flex";
+    document.getElementById("answeridol_output1").innerHTML="Tienes a un ídolo";
+    document.getElementById("answeridol_output2").innerHTML="Vives para tu "+objeto;
+    document.getElementById("answeridol_output3").innerHTML="Te haces matar por tu "+objeto;
+    document.getElementById("answeridol_output4").innerHTML="Trabajas por tu "+objeto;
+    document.getElementById("answeridol_output5").innerHTML="Tienes una mente que solo piensa en tu "+objeto;
+    document.getElementById("answeridol_output6").innerHTML="Tu corazón le pertenece a tu "+objeto;
 }
 
 function adorarDios(){
-    alert("amas a Dios!");
+    document.getElementById("answer_idol").style.display = "flex";
+    document.getElementById("answeridol_output1").innerHTML="Amas a Dios sobre todas las cosas";
+    document.getElementById("answeridol_output2").innerHTML="Das tu vida por y para Dios";
+    document.getElementById("answeridol_output3").innerHTML="Tienes un corazón integro y unificado";
+    document.getElementById("answeridol_output4").innerHTML="Trabajas por Dios y para Dios";
+    document.getElementById("answeridol_output5").innerHTML="Tienes una mente preparada para la aflicción";
+    document.getElementById("answeridol_output6").innerHTML="Todo lo anterior te permite amar en la dimensión correcta a tus padres, esposa, hijos y trabajo del mismo modo que puedes deleitarte sanamente en los placeres que Dios te ha dado en este mundo";
+}
+
+document.getElementById("answeridol_clear").onclick = function(){
+    document.getElementById("answer_idol").style.display = "none";
+    document.getElementById("question_input").value="";
 }
 
 
