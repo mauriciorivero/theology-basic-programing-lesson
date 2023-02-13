c0fc625f817984a5c3f915ea7e5975aa68cfee2c
Mauricio Rivero <rivero.mauricio@gmail.com>
2023-02-13 14:36:32 -0500

    created the initial and functional behavior of the js file

--- js/behaviour-confront.js
+++ js/behaviour-confront.js
@@ -1,1 +1,31 @@
-//document.getElementById("answer_idol").style.display="none";
+document.getElementById("answer_idol").style.display="none";
+
+
+var objetoAdoracion = "objeto";
+document.getElementById("question_button").onclick = function(){
+    objetoAdoracion = document.getElementById("question_input").value;
+    confrontarObjetoAdoracion(objetoAdoracion);
+}
+
+
+function confrontarObjetoAdoracion(objeto){
+    if(objeto===""){
+        alert("No me has dicho que amas");
+    }else if(objeto==="Dios"){
+        adorarDios();
+    }else{
+        adorarIdolo();
+    }
+}
+
+function adorarIdolo(){
+    alert("Adoras a un idolo")
+}
+
+function adorarDios(){
+    alert("amas a Dios!");
+}
+
+
+
+
